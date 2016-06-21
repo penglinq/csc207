8e9348266e553fac53ebfaa73f6462a5baa83d53
penglinq <linqingp@outlook.com>
2016-06-21 14:14:59 +0800

    finish StringExercises

--- code/2.1/the-string-class/src/StringExercises.java
+++ code/2.1/the-string-class/src/StringExercises.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class StringExercises {
 
@@ -9,9 +10,53 @@ public class StringExercises {
 		return s;
 	}
 	
+	public static boolean forgivingPrompt(String question) {
+		Scanner in = new Scanner(System.in);
+		while (true) {
+			System.out.print(question);
+			String response = in.nextLine();
+			if (response.equalsIgnoreCase("y") || response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("yep")) {
+				return true;
+			} else if (response.equalsIgnoreCase("n") || response.equalsIgnoreCase("no") || response.equalsIgnoreCase("nope")) {
+				return false;
+			} 
+				
+		}
+	}
+	
+	public static String parseName(String s) {
+		String[] arrs = new String[3];
+		int l=0, r=0;
+		for (int i=0; i<2;i++) {
+			while (s.charAt(r) != ',') {
+				r++;
+			}
+			arrs[i] = s.substring(l, r);
+			r+=1;
+			l=r;
+		}
+		arrs[2] = s.substring(r, s.length());
+		return arrs[0]+" "+arrs[1]+" "+arrs[2];
+	}
+	
+	public static int add(String line) {
+		int x = 0;
+		String[] arrs = line.split("\\+");
+		for (int i=0; i<arrs.length; i++) {
+			x += new Integer(arrs[i].trim());
+		}
+		return x;
+	}
+	
 	public static void main(String[] args) {
 		String[] a = {"ab", "cd", "ef"};
+		String q = "Are you a Grinnellian?";
+		String name = "Turing,Alan,Mathison";
+		String num = " 0+31+4+81+9+10 ";
 		System.out.println(intersperse(a));
+		System.out.println(forgivingPrompt(q));
+		System.out.println(parseName(name));
+		System.out.println(add(num));
 		return;
 	}
 
